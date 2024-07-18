82a7396f5d13501358df0374f46513f65a5b8446
ankita_sutar02-admin <ankitavsutar2000@gmail.com>
2024-07-18 14:25:01 +0530

    ldap integration with spring boot

---
+++ webSecure/webSecure/src/main/java/com/Project/webSecure/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.Project.webSecure.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Welcome to the home page!";
+  }
+
+}
---
+++ webSecure/webSecure/src/main/java/com/Project/webSecure/WebSecureApplication.java
@@ -0,0 +1,13 @@
+package com.Project.webSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecureApplication.class, args);
+	}
+
+}
